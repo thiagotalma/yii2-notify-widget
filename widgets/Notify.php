7cdf915f9570c642e55cd1e704dc43238dafd585
Thiago Talma <thiago@thiagomt.com>
2014-03-06 19:17:39 -0300

    Display Flash Messages

--- widgets/Notify.php
+++ widgets/Notify.php
@@ -21,6 +21,25 @@ use yii\web\JsExpression;
 class Notify extends \yii\bootstrap\Widget
 {
 	/**
+	 * @var array the alert types configuration for the flash messages.
+	 * This array is setup as $key => $value, where:
+	 * - $key is the name of the session flash variable
+	 * - $value is the bootstrap alert type (i.e. danger, success, info, warning)
+	 */
+	public $alertTypes = [
+		'error'   => 'alert-danger',
+		'danger'  => 'alert-danger',
+		'success' => 'alert-success',
+		'info'    => 'alert-info',
+		'warning' => 'alert-warning'
+	];
+
+	/**
+	 * @var array the options for rendering the close button tag.
+	 */
+	public $closeButton = [];
+
+	/**
 	 * @var string Position of the notify bubble
 	 */
 	public $position = 'top-right';
@@ -71,6 +90,26 @@ class Notify extends \yii\bootstrap\Widget
 		parent::init();
 		$this->registerAssets();
 		echo Html::tag('div', '', ['id' => 'talmaNotify', 'class' => "notifications {$this->position}"]);
+
+		$session = \Yii::$app->getSession();
+		$flashes = $session->getAllFlashes();
+		$appendCss = isset($this->options['class']) ? ' ' . $this->options['class'] : '';
+
+		foreach ($flashes as $type => $message) {
+			/* initialize css class for each alert box */
+			$this->options['class'] = $this->alertTypes[$type] . $appendCss;
+
+			/* assign unique id to each alert box */
+			$this->options['id'] = $this->getId() . '-' . $type;
+
+			echo \yii\bootstrap\Alert::widget([
+				'body' => $message,
+				'closeButton' => $this->closeButton,
+				'options' => $this->options,
+			]);
+
+			$session->removeFlash($type);
+		}
 	}
 
 	/**
