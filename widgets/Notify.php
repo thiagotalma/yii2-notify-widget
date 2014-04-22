29847b02baf9a42e4fef8227fa536c9ad73323f6
Thiago Talma <thiago@thiagomt.com>
2014-04-22 15:58:19 -0300

    adjust phpdc

--- widgets/Notify.php
+++ widgets/Notify.php
@@ -7,6 +7,9 @@
  */
 
 namespace talma\widgets;
+
+use yii\bootstrap\Alert;
+use yii\bootstrap\Widget;
 use yii\helpers\Html;
 use yii\helpers\Json;
 use yii\web\JsExpression;
@@ -18,125 +21,124 @@ use yii\web\JsExpression;
  * @since 1.0
  * @see https://github.com/goodybag/bootstrap-notify
  */
-class Notify extends \yii\bootstrap\Widget
+class Notify extends Widget
 {
-	/**
-	 * @var array the alert types configuration for the flash messages.
-	 * This array is setup as $key => $value, where:
-	 * - $key is the name of the session flash variable
-	 * - $value is the bootstrap alert type (i.e. danger, success, info, warning)
-	 */
-	public $alertTypes = [
-		'error'   => 'alert-danger',
-		'danger'  => 'alert-danger',
-		'success' => 'alert-success',
-		'info'    => 'alert-info',
-		'warning' => 'alert-warning'
-	];
+    /**
+     * @var array the alert types configuration for the flash messages.
+     * This array is setup as $key => $value, where:
+     * - $key is the name of the session flash variable
+     * - $value is the bootstrap alert type (i.e. danger, success, info, warning)
+     */
+    public $alertTypes = [
+        'error'   => 'alert-danger',
+        'danger'  => 'alert-danger',
+        'success' => 'alert-success',
+        'info'    => 'alert-info',
+        'warning' => 'alert-warning'
+    ];
 
-	/**
-	 * @var array the options for rendering the close button tag.
-	 */
-	public $closeButton = [];
+    /**
+     * @var array the options for rendering the close button tag.
+     */
+    public $closeButton = [];
 
-	/**
-	 * @var string Position of the notify bubble
-	 */
-	public $position = 'top-right';
+    /**
+     * @var string Position of the notify bubble
+     */
+    public $position = 'top-right';
 
-	/**
-	 * @var string Alert style, omit alert- from style name.
-	 */
-	public $type = 'success';
+    /**
+     * @var string Alert style, omit alert- from style name.
+     */
+    public $type = 'success';
 
-	/**
-	 * @var boolean Allow alert to be closable through a close icon.
-	 */
-	public $closable = true;
+    /**
+     * @var boolean Allow alert to be closable through a close icon.
+     */
+    public $closable = true;
 
-	/**
-	 * @var array Alert transition, pretty sure only fade is supported, you can try others if you wish.
-	 */
-	public $transition = 'fade';
+    /**
+     * @var array Alert transition, pretty sure only fade is supported, you can try others if you wish.
+     */
+    public $transition = 'fade';
 
-	/**
-	 * @var array Fade alert out after a certain delay (in ms)
-	 */
-	public $fadeOut = [
-		'enabled' => true,
-		'delay' => 3000
-	];
+    /**
+     * @var array Fade alert out after a certain delay (in ms)
+     */
+    public $fadeOut = [
+        'enabled' => true,
+        'delay' => 3000
+    ];
 
-	/**
-	 * @var array Text to show on alert, you can use either html or text. HTML will override text.
-	 */
-	public $message = null;
+    /**
+     * @var array Text to show on alert, you can use either html or text. HTML will override text.
+     */
+    public $message = null;
 
-	/**
-	 * @var string Text to show on alert, you can use either html or text. HTML will override text.
-	 */
+    /**
+     * @var string Text to show on alert, you can use either html or text. HTML will override text.
+     */
     public $onClose = 'function () {}';
 
-	/**
-	 * @var string Called before alert closes.
-	 */
+    /**
+     * @var string Called before alert closes.
+     */
     public $onClosed = 'function () {}';
 
-	/**
-	 * @inheritdoc
-	 */
-	public function init()
-	{
-		parent::init();
-		$this->registerAssets();
-		echo Html::tag('div', '', ['id' => 'talmaNotify', 'class' => "notifications {$this->position}"]);
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        $this->registerAssets();
+        echo Html::tag('div', '', ['id' => 'talmaNotify', 'class' => "notifications {$this->position}"]);
 
-		$session = \Yii::$app->getSession();
-		$flashes = $session->getAllFlashes();
-		$appendCss = isset($this->options['class']) ? ' ' . $this->options['class'] : '';
+        $session = \Yii::$app->getSession();
+        $flashes = $session->getAllFlashes();
+        $appendCss = isset($this->options['class']) ? ' ' . $this->options['class'] : '';
 
-		foreach ($flashes as $type => $message) {
-			/* initialize css class for each alert box */
-			$this->options['class'] = $this->alertTypes[$type] . $appendCss;
+        foreach ($flashes as $type => $message) {
+            /* initialize css class for each alert box */
+            $this->options['class'] = $this->alertTypes[$type] . $appendCss;
 
-			/* assign unique id to each alert box */
-			$this->options['id'] = $this->getId() . '-' . $type;
+            /* assign unique id to each alert box */
+            $this->options['id'] = $this->getId() . '-' . $type;
 
-			echo \yii\bootstrap\Alert::widget([
-				'body' => $message,
-				'closeButton' => $this->closeButton,
-				'options' => $this->options,
-			]);
+            echo Alert::widget([
+                'body' => $message,
+                'closeButton' => $this->closeButton,
+                'options' => $this->options,
+            ]);
 
-			$session->removeFlash($type);
-		}
-	}
+            $session->removeFlash($type);
+        }
+    }
 
-	/**
-	 * Registers the needed assets
-	 */
-	public function registerAssets()
-	{
-		$view = $this->getView();
-		NotifyAsset::register($view);
+    /**
+     * Registers the needed assets
+     */
+    public function registerAssets()
+    {
+        $view = $this->getView();
+        NotifyAsset::register($view);
 
-		$config = [
-			'type' => $this->type,
-			'closable' => $this->closable,
-			'transition' => $this->transition,
-			'fadeOut' => $this->fadeOut,
-			'message' => $this->message,
-			'onClose' => new JsExpression($this->onClose),
-			'onClosed' => new JsExpression($this->onClosed)
-		];
-		$defaults = Json::encode($config);
+        $config = [
+            'type' => $this->type,
+            'closable' => $this->closable,
+            'transition' => $this->transition,
+            'fadeOut' => $this->fadeOut,
+            'message' => $this->message,
+            'onClose' => new JsExpression($this->onClose),
+            'onClosed' => new JsExpression($this->onClosed)
+        ];
+        $defaults = Json::encode($config);
 
-		$js = <<<SCRIPT
+        $js = <<<SCRIPT
 ;(function($, window, document, undefined) {
-	$('.alert').toNotify({$defaults}, $('#talmaNotify')).show();
+    $('.alert').toNotify({$defaults}, $('#talmaNotify')).show();
 })(window.jQuery, window, document);
 SCRIPT;
-		$view->registerJs($js);
-	}
-
+        $view->registerJs($js);
+    }
 }
--- widgets/NotifyAsset.php
+++ widgets/NotifyAsset.php
@@ -18,49 +18,51 @@ use yii\web\AssetBundle;
  */
 class NotifyAsset extends AssetBundle
 {
-	public $depends = [
-		'yii\web\JqueryAsset',
-	];
+    /**
+     * @inheritdoc
+     */
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
 
-	/**
-	 * Set up CSS and JS asset arrays based on the base-file names
-	 * @param string $type whether 'css' or 'js'
-	 * @param array $files the list of 'css' or 'js' basefile names
-	 */
-	protected function setupAssets($type, $files = [])
-	{
-		$srcFiles = [];
-		$minFiles = [];
-		foreach ($files as $file) {
-			$srcFiles[] = "{$file}.{$type}";
-			$minFiles[] = "{$file}.min.{$type}";
-		}
-		if (empty($this->$type)) {
-			//$this->$type = YII_DEBUG ? $srcFiles : $minFiles;
-			$this->$type = $srcFiles;
-		}
-	}
+    /**
+     * Set up CSS and JS asset arrays based on the base-file names
+     * @param string $type whether 'css' or 'js'
+     * @param array $files the list of 'css' or 'js' basefile names
+     */
+    protected function setupAssets($type, $files = [])
+    {
+        $srcFiles = [];
+        $minFiles = [];
+        foreach ($files as $file) {
+            $srcFiles[] = "{$file}.{$type}";
+            $minFiles[] = "{$file}.min.{$type}";
+        }
+        if (empty($this->$type)) {
+            //$this->$type = YII_DEBUG ? $srcFiles : $minFiles;
+            $this->$type = $srcFiles;
+        }
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public function init()
-	{
-		$this->setSourcePath(__DIR__ . '/../assets');
-		$this->setupAssets('css', ['css/bootstrap-notify']);
-		$this->setupAssets('js', ['js/bootstrap-notify']);
-		parent::init();
-	}
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        $this->setSourcePath(__DIR__ . '/../assets');
+        $this->setupAssets('css', ['css/bootstrap-notify']);
+        $this->setupAssets('js', ['js/bootstrap-notify']);
+        parent::init();
+    }
 
-	/**
-	 * Sets the source path if empty
-	 * @param string $path the path to be set
-	 */
-	protected function setSourcePath($path)
-	{
-		if (empty($this->sourcePath)) {
-			$this->sourcePath = $path;
-		}
-	}
-
+    /**
+     * Sets the source path if empty
+     * @param string $path the path to be set
+     */
+    protected function setSourcePath($path)
+    {
+        if (empty($this->sourcePath)) {
+            $this->sourcePath = $path;
+        }
+    }
 }
